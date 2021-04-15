1d13696204190af62c5d80b77fc3fe0278746b00
dfacina <32079000+dfacina@users.noreply.github.com>
2021-04-15 16:12:21 -0300

    Create Circulo.java

---
+++ Circulo.java
@@ -0,0 +1,19 @@
+package entidades;
+
+public class Circulo extends Forma{
+
+    private Double raio;
+
+    public Circulo(Double raio){
+        super();
+        this.raio = raio;
+    }
+
+    @Override
+    public Double getArea(){
+
+        return Math.PI * raio * raio;
+    }
+
+    
+}
